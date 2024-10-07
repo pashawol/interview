9f5c1a9b569a74c094ba8ecb75ed7384fe28ce1b
PAVEL KHALUNIN <wol1414@gmail.com>
2024-10-07 14:21:10 +0200

    feat: add questions

--- common.js
+++ common.js
@@ -7,3 +7,7 @@ function outerFunction(outerVariable) {
 
 const newFunction = outerFunction;
 newFunction('inside')('outside');
+
+const regex = /(\d{4})-(\d{2})-(\d{2})/;
+const result = regex.exec('2023-10-05');
+console.log(result); // ["2023-10-05", "2023", "10", "05", index: 0, input: "2023-10-05", groups: undefined]
